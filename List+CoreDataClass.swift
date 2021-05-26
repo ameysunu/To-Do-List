58bbb58c1c3caa4d798f37bb8108adb668a1cf1a
ameysunu <ameysunu00719@gmail.com>
2021-05-26 12:29:33 +0300

    persistence added

---
+++ List+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  List+CoreDataClass.swift
+//  ToDo
+//
+//  Created by Amey Sunu on 26/05/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(List)
+public class List: NSManagedObject {
+
+}
